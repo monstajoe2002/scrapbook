79a7b04633728dd08311ec9ffc92df7f51a822dd
monstajoe2002 <youssef.amr90@gmail.com>
2025-04-04 23:26:22 +0200

    setup supabase locally

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -4,12 +4,15 @@ import { syncedSupabase } from "@legendapp/state/sync-plugins/supabase";
 import { observablePersistAsyncStorage } from "@legendapp/state/persist-plugins/async-storage";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { observable } from "@legendapp/state";
+import "react-native-get-random-values";
+import { v4 as uuidv4 } from "uuid";
 const supabase = createClient(
   process.env.EXPO_PUBLIC_SUPABASE_URL!,
   process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!
 );
-// create a configured sync function
 
+const generateId = () => uuidv4();
+// create a configured sync function
 const customSynced = configureSynced(syncedSupabase, {
   persist: {
     plugin: observablePersistAsyncStorage({ AsyncStorage }),
