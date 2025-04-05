239c3ef97fc7820795fa206828883289a16b378c
monstajoe2002 <youssef.amr90@gmail.com>
2025-04-05 22:08:46 +0200

    chore: remove unused linking logic from HomeScreen component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
-import * as Linking from "expo-linking";
 
 const HomeScreen = () => {
-  const url = Linking.useURL();
-  if (url) {
-    const { hostname, path, queryParams } = Linking.parse(url);
-    console.log(
-      `Linked to app with hostname: ${hostname}, path: ${path} and data: ${JSON.stringify(
-        queryParams
-      )}`
-    );
-  }
   return (
     <View>
       <Text>Hello, World!</Text>
